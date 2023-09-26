e234c892db56587e52091bd7ca8d38e8163011ca
Luqmaan Hakeem <rockgameplayhakeem@gmail.com>
2023-09-26 22:30:43 +0530

    code: Created a Pause UI

---
+++ DrizzleKitchen/Assets/Scripts/UI/PauseUI.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseUI : MonoBehaviour
+{
+     [SerializeField] private Button resumeButton;
+     [SerializeField] private Button mainMenuButton;
+
+     private void Awake(){
+        resumeButton.onClick.AddListener(()=> {
+            GameManager.Instance.TogglePauseGame();
+        });
+        mainMenuButton.onClick.AddListener(()=>{
+            SceneManager.LoadScene(0);
+        });
+     }
+     private void Start(){
+        GameManager.Instance.OnGamePaused+= GameManager_OnGamePaused;
+        GameManager.Instance.OnGameUnPaused += GameManager_OnUnpaused;
+        Hide();
+    }
+    private void GameManager_OnGamePaused(object sender , System.EventArgs e){
+       Show();
+    }
+    private void GameManager_OnUnpaused(object sender , System.EventArgs e){
+        Hide();
+    }
+    private void Show(){
+        gameObject.SetActive(true);
+    }
+    private void Hide(){
+        gameObject.SetActive(false);
+    }
+}
+

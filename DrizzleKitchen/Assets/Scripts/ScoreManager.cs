758fae69548a35ab5ba7df7a30360f4afba946cd
Luqmaan Hakeem <rockgameplayhakeem@gmail.com>
2023-10-29 15:26:53 +0530

    code: fixed a bug with scores
    Bug: the score kept increasing until the game ended.

--- DrizzleKitchen/Assets/Scripts/ScoreManager.cs
+++ DrizzleKitchen/Assets/Scripts/ScoreManager.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
+    public static ScoreManager Instance {get;private set;}
      [SerializeField] private NpcInteractable npc; 
      [SerializeField] private OrderState orderstate;
-     float score =0;
+     private float score;
+     public float money;
+     
+     private void Awake(){
+        Instance = this;
+
+     }
      private void Start(){
         score =0;
      }
@@ -14,13 +21,19 @@ public class ScoreManager : MonoBehaviour
     private void Update(){
         npc.OnDelivered += Score_OnDelivered;
     }
-    private void Score_OnDelivered(object sender , System.EventArgs e){
+    public void Score_OnDelivered(object sender , System.EventArgs e){
         if(npc.delivered){
             DeliveryRecipeSO order = orderstate.Order;
             Debug.Log(order.cost);
             score += order.cost;
             Debug.Log(score);
+            money = score;
+            npc.delivered = false;
+    
         }   
+    }
+    public float GetScore(){
+        return money;
     }
 
 }
